3263e1201e84611396e620f021f43d133e79f0a2
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2021-07-16 11:10:04 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Input } from '@chakra-ui/react'
+import { useState } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = async () => {
+    const res = await axios.post('http://192.168.0.10:3333', {
+      username: username,
+      hash: password,
+    })
+    console.log(res)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Input
+        placeholder="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <Input
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button onClick={handleLogin}>Login</Button>
+    </>
+  )
 }
 
-export default App;
+export default App
